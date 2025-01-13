d49e57ce5de3609db1d1b508aab75b385ad3022b
مهدی فرهانی <m.farhani@entekhabgroup.ir>
2025-01-13 04:49:37 +0330

    Evaluation Export

---
+++ Models/EvaluationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IAUN.InformationRetrieval.Final.Models;
+
+public class EvaluationModel
+{
+	
+	public int QueryIndex { get; set; }
+	public string Result { get; set; } = string.Empty;
+	public decimal Precision { get; set; }
+	public decimal ReCall { get; set; }
+	public decimal FMeasure { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@ await parser.LoadFile(booleanQueries);
 var queryIndex = 0;
 Console.WriteLine(new string('-', 20));
 var allRetrieved = new List<List<PostingListInfo>>();
+var evaluation = new List<EvaluationModel>();
+
 parser.Quires.ForEach(async query =>
 {
 
@@ -49,31 +51,52 @@ parser.Quires.ForEach(async query =>
 
 	var fMeasure = documents.FMeasure(result, queryIndex);
 	Console.WriteLine($"F-Measure for query #{queryIndex} = {fMeasure:F4}");
-
+	evaluation.Add(new EvaluationModel
+	{
+		QueryIndex = queryIndex,
+		FMeasure = fMeasure,
+		Precision = precision,
+		ReCall = recall,
+		Result = founded??""
+	});
 	sw.Stop();
 	Console.WriteLine($"result in {sw.ElapsedMilliseconds} ms.");
 	Console.WriteLine(new string('-', 20));
 });
 
 #endregion
+SaveResult("booleanResults", evaluation);
+
+void SaveResult(string name,List<EvaluationModel> evaluation)
+{
+	using var file = new StreamWriter($"{name}.csv");
+	file.WriteLine("Query Number ,  Precision ,  ReCall ,  F-Measure ,  Result");
+	evaluation.ForEach(index =>
+	{
+
+		file.WriteLine($"{index.QueryIndex},{index.Precision},{index.ReCall},{index.FMeasure}," +
+			$"{index.Result.Replace(",","-")}");
+	});
+}
+
 var map = documents.MAP(allRetrieved!);
 Console.WriteLine($"MAP for all queries = {map:F4}");
 
 
-Console.WriteLine($"{new string('-', 20)} BERT MODEL {new string('-', 20)}");
 
 #region BERT
+Console.WriteLine($"{new string('-', 20)} BERT MODEL {new string('-', 20)}");
 var bertProcessor = new BertEmbeddings(sourcePath, relPath, vocab, onnx, queryPath, database);
 await bertProcessor.PrepareDocuments();
 queryIndex = 0;
 var allBertRetrieved = new List<List<DocumentSimilarity>>();
-
+evaluation = new List<EvaluationModel>();
 bertProcessor.Queries.ForEach(query =>
 {
 	queryIndex++;
 	var output = bertProcessor.Search(query, queryIndex);
 	allBertRetrieved.Add(output);
-	
+
 	foreach (var item in output.Take(10).ToList())
 	{
 		Console.WriteLine($"{item.DocumentId} ---- {item.Similarity}");
@@ -86,9 +109,17 @@ bertProcessor.Queries.ForEach(query =>
 
 	var fMeasure = bertProcessor.FMeasure(output, queryIndex);
 	Console.WriteLine($"F-Measure for query #{queryIndex} = {fMeasure:F4}");
-
+	evaluation.Add(new EvaluationModel
+	{
+		QueryIndex = queryIndex,
+		FMeasure = fMeasure,
+		Precision = precision,
+		ReCall = recall,
+		Result = output.Select(x => $"({x.DocumentId}={x.Similarity})").Aggregate((x, c) => x + "-" + c)
+	});
 	Console.WriteLine(new string('-', 20));
 });
+SaveResult("BERT-Result", evaluation);
 map = bertProcessor.MAP(allBertRetrieved!);
 Console.WriteLine($"MAP for all queries = {map:F6}");
 Console.WriteLine(new string('-', 20));
